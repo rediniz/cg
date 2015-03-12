3a2974abfa7ae2ac91cd5ee5360f6d6666fa2b70
RenatoD <RenatoD@10.0.1.6>
2015-03-12 14:52:02 -0300

    Teste

---
+++ Cipher/src/CipherWriter.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+import java.io.Writer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RenatoD
+ */
+public class CipherWriter extends Writer{
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void flush() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
